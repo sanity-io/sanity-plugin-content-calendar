b935def46e0df09a4c8d8b62f5a448285785ec70
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2022-09-15 21:27:22 +0200

    fix: use new useClient api

--- src/client.ts
+++ src/client.ts
@@ -1,13 +1,5 @@
-import {useMemo} from 'react'
 import {useClient} from 'sanity'
 
 export function useSanityClient(apiVersion?: string) {
-  const client = useClient()
-  return useMemo(
-    () =>
-      client.withConfig({
-        apiVersion: apiVersion ?? '2021-09-06'
-      }),
-    [client, apiVersion]
-  )
+  return useClient({apiVersion: apiVersion ?? '2021-09-06'})
 }
